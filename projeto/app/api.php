8d73975c37bb1301d27a8a92cd18ed40b6eefe46
Guilherme sandoval <guijpjs@gmail.com>
2020-01-31 17:58:01 -0300

    Criada Função para resgatar e validar Token

--- projeto/app/api.php
+++ projeto/app/api.php
@@ -4,13 +4,14 @@ header("Access-Control-Allow-Origin: * ");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
-// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 require_once('../../vendor/autoload.php');
 require('connection.php');
 
 
 use \Firebase\JWT\JWT;
+
 define('SECRET_KEY', base64_encode('3e018c951f88d0e7d1ed3c7bcf94d938SEJUS2020'));  //secret key para fazer a validação do token
 define('ALGORITHM', 'HS256');   // tipo do algoritmo do token 
 
@@ -20,12 +21,13 @@ $request = json_decode($postdata);
 
 
 $action = $request->action;
+
 // Seção de Login
 if ($action == 'login') {
 
     $username = $request->username;
 
-    //Buscar informações dos Usuários no Banco
+    //Buscar informações dos Usuários no Banco de Dados
     try {
         $sql = "SELECT U.*, T.desc_tipo_usuario, S.desc_sede FROM tb_usuario AS U
         INNER JOIN tb_tipo_usuario AS T ON U.id_tipo_usuario = T.id_tipo_usuario
@@ -33,29 +35,41 @@ if ($action == 'login') {
         $obj = $connect->prepare($sql);
         $obj->execute();
         $user = $obj->fetch(PDO::FETCH_ASSOC);
-        
     } catch (Exception $e) {
         echo 'Erro: ', $e->getMessage(), "\n";
     }
-    
-    //Verificar se username ou cpf é compatível com a senha e se senha está correta
-    if (($username == $user['usuario'] || $username == $user['id_cpf'])  && password_verify($request->password,$user['hashSenha'])) {
-        $iat = time(); // Tempo que o tokenn foi gertado
-        $nbf = $iat + 1; //not before in seconds
-        $exp = $iat + 120; // Tempo em que o token será expirado
-        $iss = $_SERVER['REQUEST_URI'];
+
+    //  Verificar se username ou cpf é compatível com o usuário e se senha está correta
+    if (($username == $user['usuario'] || $username == $user['id_cpf'])  && password_verify($request->password, $user['hashSenha'])) {
+        $iat = time();  //  Timestamp de quando o token foi criado
+        $nbf = $iat + 1; //  Timestamp de quando o token não pode ter sido criado
+        $exp = $iat + 3600; //  Timestamp de quando o token irá expirar
+        $iss = $_SERVER['REQUEST_URI']; //  Emissor do token
+        $aud = "http://localhost:4200/#/visitantes"; // Destinatário do token, representa a aplicação que irá usá-lo.
+
+        $id_cpf = $user['id_cpf'];
         $nome = $user['nome'];
+        $usuario = $user['usuario'];
+        $email = $user['email'];
+        $telefone = $user['telefone'];
+        $id_tipo_usuario = $user['id_tipo_usuario'];
+        $id_sede = $user['id_sede'];
+
         $token = array(
-            // "iss" => "http://example.org",
             "iss" => $iss,
-            // "aud" => "http://example.com",
-            "aud" => $iss,
+            "aud" => $aud,
             "iat" => $iat,
             "nbf" => $nbf,
             "exp" => $exp,
             "data" => array(
-                "id" => 11,
-                "email" => $nome
+                "id_cpf"            =>  $id_cpf,
+                "nome"              =>  $nome,
+                "usuario"           =>  $usuario,
+                "email"             =>  $email,
+                "telefone"          =>  $telefone,
+                "id_tipo_usuario"   =>  $id_tipo_usuario,
+                "id_sede"           =>  $id_sede
+
             )
         );
 
@@ -66,11 +80,9 @@ if ($action == 'login') {
 
         $data_insert = array(
             'access_token' => $jwt,
-            'id'   => '007',
-            'name' => 'guilherme Sandoval Veras',
+            'name' => $nome,
             'time' => time(),
-            'username' => 'FreakyJolly',
-            'email' => 'contact@freakyjolly.com',
+            'email' => $email,
             'status' => "success",
             'message' => "Login Realizado com Sucesso, Olá " . $nome . ""
         );
@@ -81,11 +93,86 @@ if ($action == 'login') {
             "message" => "Usuário ou Senha Inválidos"
         );
     }
-    // $_SERVER['HTTP_AUTHORIZATION'] = $jwt;
 }
-// Get Dashboard stuff
+
+// Validar Token recebido do Front-end
 else if ($action == 'stuff') {
+
     $authHeader = $request->token;
+    // $authHeader = getBearerToken();
+    $temp_header = explode(".", $authHeader);
+
+    $header = $temp_header[0];
+    $payload = $temp_header[1];
+    $signature = $temp_header[2];
+
+    try {
+        JWT::$leeway = 10;
+
+        $decoded = JWT::decode($authHeader, SECRET_KEY, array(ALGORITHM));
+
+        $data_insert = array(
+            // "data" => json_decode($data_from_server),
+            "status" => "success",
+            "message" => "Request authorized"
+        );
+    } catch (Exception $e) {
+
+        http_response_code(401);
+
+        $data_insert = array(
+            "jwt" => $authHeader,
+            "status" => "error",
+            "message" => $e->getMessage()
+        );
+    }
+} else if ($action = 'teste') {
+    $data_insert = validToken($request);
+}
+
+
+echo json_encode($data_insert);
+
+
+
+//Resgatar Token do Header 
+function getAuthorizationHeader()
+{
+    $headers = null;
+    if (isset($_SERVER['Authorization'])) {
+        $headers = trim($_SERVER["Authorization"]);
+    } else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+        $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+    } elseif (function_exists('apache_request_headers')) {
+        $requestHeaders = apache_request_headers();
+        // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+        $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+        //print_r($requestHeaders);
+        if (isset($requestHeaders['Authorization'])) {
+            $headers = trim($requestHeaders['Authorization']);
+        }
+    }
+    return $headers;
+}
+function getBearerToken()
+{
+    $headers = getAuthorizationHeader();
+    // HEADER: Get the access token from the header
+    if (!empty($headers)) {
+        if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+            return $matches[1];
+        }
+    }
+    return null;
+}
+//Fim da função de resgatar token do header
+
+//Função de validação do Token
+function validToken($request)
+{
+
+    $authHeader = $request->token;
+    // $authHeader = getBearerToken();
     $temp_header = explode(".", $authHeader);
 
     $header = $temp_header[0];
@@ -98,13 +185,13 @@ else if ($action == 'stuff') {
         $decoded = JWT::decode($authHeader, SECRET_KEY, array(ALGORITHM));
 
 
-        // Access is granted. Add code of the operation here 
+        // Se o token não foi modificado ou se não expirou ele será válido
 
-        $data_from_server = '{"Coords":[{"Accuracy":"65","Latitude":"53.277720488429026","Longitude":"-9.012038778269686","Timestamp":"Fri Jul 05 2013 11:59:34 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.277720488429026","Longitude":"-9.012038778269686","Timestamp":"Fri Jul 05 2013 11:59:34 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.27770755361785","Longitude":"-9.011979642121824","Timestamp":"Fri Jul 05 2013 12:02:09 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.27769091555766","Longitude":"-9.012051410095722","Timestamp":"Fri Jul 05 2013 12:02:17 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.27769091555766","Longitude":"-9.012051410095722","Timestamp":"Fri Jul 05 2013 12:02:17 GMT+0100 (IST)"}]}';
+        // $data_from_server = '{"Coords":[{"Accuracy":"65","Latitude":"53.277720488429026","Longitude":"-9.012038778269686","Timestamp":"Fri Jul 05 2013 11:59:34 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.277720488429026","Longitude":"-9.012038778269686","Timestamp":"Fri Jul 05 2013 11:59:34 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.27770755361785","Longitude":"-9.011979642121824","Timestamp":"Fri Jul 05 2013 12:02:09 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.27769091555766","Longitude":"-9.012051410095722","Timestamp":"Fri Jul 05 2013 12:02:17 GMT+0100 (IST)"},{"Accuracy":"65","Latitude":"53.27769091555766","Longitude":"-9.012051410095722","Timestamp":"Fri Jul 05 2013 12:02:17 GMT+0100 (IST)"}]}';
 
 
-        $data_insert = array(
-            "data" => json_decode($data_from_server),
+        $result = array(
+            // "data" => json_decode($data_from_server),
             "status" => "success",
             "message" => "Request authorized"
         );
@@ -112,58 +199,12 @@ else if ($action == 'stuff') {
 
         http_response_code(401);
 
-        $data_insert = array(
-            //"data" => $data_from_server,
+        $result = array(
             "jwt" => $authHeader,
             "status" => "error",
             "message" => $e->getMessage()
         );
+        return $result;
     }
+
 }
-// } else if ($action == 'valid') {
-
-
-
-//     $authHeader = $request->token;
-//     // echo 'authheader: ',$authHeader;
-//     $temp_header = explode(".", $authHeader);
-//     print_r($temp_header);
-//     $jwt = $temp_header[0];
-//     echo $jwt;
-
-//     // echo $token;
-//     exit;
-//     $part = explode(".", $token);
-//     $header = $part[0];
-//     $payload = $part[1];
-//     $signature = $part[2];
-
-//     // echo 'Header = ', $header, '<br>';
-//     // echo 'Payload = ', $payload, '<br>';
-//     // echo 'Signature = ', $signature, '<br>';
-
-//     $valid = hash_hmac('sha256', "$header.$payload", SECRET_KEY, true);
-//     $valid = base64_encode($valid);
-
-
-//     echo 'Secret key ', $valid, '<br>';
-//     echo 'signature ', $signature;
-//     // echo $valid;
-//     if (SECRET_KEY == $signature) {
-//         echo " valid";
-//     } else {
-//         echo ' invalid';
-//     }
-//     exit;
-// }
-
-echo json_encode($data_insert);
-// print_r( $data_insert);
-// print_r (json_encode($data_insert));
-
-
-
-        // exi
-    
-
-// echo json_encode($data_insert);
